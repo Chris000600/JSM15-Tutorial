ce6641aa2c3c104677b5e9580f1aca822697f6c7
Chris000600 <82931875+Chris000600@users.noreply.github.com>
2024-12-17 14:09:30 +0700

    navbar github signin feature

---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-3xl font-[family-name:var(--font-josefin-sans)]">
+        Welcome to Next.js
+      </h1>
+    </>
+  );
+}
--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  console.log('What am I? -- SERVER/CLIENT?');
-
-  return (
-    <h1 className="text-3xl font-[family-name:var(--font-josefin-sans)]">
-      Welcome to Next.js
-    </h1>
-  );
-}
